e5c202a00792f0b3f8ce7227ecbd37907b92622c
Eunki Hong <h.pichulia@gmail.com>
2023-01-06 03:18:13 +0900

    Support unsigned/long long and double format I/O
    Signed-off-by: Eunki Hong <h.pichulia@gmail.com>

--- src/1_FastIO/fastio-potable.cpp
+++ src/1_FastIO/fastio-potable.cpp
@@ -14,7 +14,7 @@ public:
 	int pos;
 	FILE* fp;
 	int pl;
-	int eof;
+	int __eof;
 
 	// Customize yourselfs
 	static inline bool isEmpty(char x){
@@ -32,18 +32,18 @@ private:
 		pos = BUFFER_MAX;
 		pl = BUFFER_MAX;
 		fp = stdin;
-		eof = 0;
+		__eof = 0;
 	}
 	void read_buffer() {
-		if (eof)return;
-		if (feof(fp)) { eof = 1; return; }
+		if (__eof)return;
+		if (feof(fp)) { __eof = 1; return; }
 		pl = fread(buf, sizeof(char), BUFFER_MAX, fp);
 	}
 	char __getchar() {
 		if (pos == pl) {
 			pos = 0;
 			read_buffer();
-			if (eof)return -1;
+			if (__eof)return -1;
 		}
 		return buf[pos++];
 	}
@@ -52,16 +52,117 @@ private:
 		char c;
 		bool minus = false;
 		do {
-			c = getchar();
-			if (eof)return 0;
+			c = __getchar();
+			if (__eof)return 0;
 			if (c == '-')minus = true;
 		} while (c < '0' || c>'9');
 		res = c - '0';
 		while (1) {
-			c = getchar();
-			if (eof)break;
+			c = __getchar();
+			if (__eof)break;
 			if (c < '0' || c>'9')break;
 			res = (res * 10) + c - '0';
+		}
+		return minus ? -res : res;
+	}
+	
+	unsigned int __nextuint() {
+		unsigned int res = 0;
+		char c;
+		do {
+			c = __getchar();
+			if (__eof)return 0;
+		} while (c < '0' || c>'9');
+		res = c - '0';
+		while (1) {
+			c = __getchar();
+			if (__eof)break;
+			if (c < '0' || c>'9')break;
+			res = (res * 10) + c - '0';
+		}
+		return res;
+	}
+	
+	long long int __nextlld() {
+		long long int res = 0;
+		char c;
+		bool minus = false;
+		do {
+			c = __getchar();
+			if (__eof)return 0;
+			if (c == '-')minus = true;
+		} while (c < '0' || c>'9');
+		res = c - '0';
+		while (1) {
+			c = __getchar();
+			if (__eof)break;
+			if (c < '0' || c>'9')break;
+			res = (res * 10) + c - '0';
+		}
+		return minus ? -res : res;
+	}
+	
+	unsigned long long int __nextulld() {
+		unsigned long long int res = 0;
+		char c;
+		do {
+			c = __getchar();
+			if (__eof)return 0;
+		} while (c < '0' || c>'9');
+		res = c - '0';
+		while (1) {
+			c = __getchar();
+			if (__eof)break;
+			if (c < '0' || c>'9')break;
+			res = (res * 10) + c - '0';
+		}
+		return res;
+	}
+	
+	double __nextlf() {
+		double res = 0;
+		double base = 1;
+		bool minus = false;
+		char c;
+		do {
+			c = __getchar();
+			if (__eof)return 0;
+			if(c == '-'){
+				minus = true;
+				continue;
+			}
+		} while ((c < '0' || c>'9') && (c != '.'));
+		bool meet_point = false;
+		if(c >= '0' && c<='9')
+		{
+			res = c - '0';
+		}
+		else{
+			meet_point = true;
+		}
+		while (1) {
+			c = __getchar();
+			if (__eof)break;
+			if(c == '.'){
+				meet_point = true;
+				continue;
+			}
+			if(c == 'e' || c == 'E'){
+				int sft = __nextint();
+				while(sft != 0){
+					if(sft > 0) {res *= 10; sft--;}
+					if(sft < 0) {res *= 0.1; sft++;}
+				}
+				break;
+			}
+			if (c < '0' || c>'9')break;
+			if(meet_point){
+				base *= 0.1;
+				res = res + base * (c - '0');
+			}
+			else{
+				res = (res * 10) + c - '0';
+			}
 		}
 		return minus ? -res : res;
 	}
@@ -70,13 +171,13 @@ private:
 		int i = 0;
 		char c;
 		do {
-			c = getchar();
-			if (eof)return 0;
+			c = __getchar();
+			if (__eof)return 0;
 		} while (isEmpty(c));
 		x[i++] = c;
 		while (1) {
-			c = getchar();
-			if (eof)break;
+			c = __getchar();
+			if (__eof)break;
 			if (isEmpty(c))break;
 			x[i++] = c;
 		}
@@ -92,11 +193,26 @@ public:
 	static void setfp(FILE* _fp) {
 		Get().fp = _fp;
 	}
+	static bool eof() {
+		return Get().__eof;
+	}
 	static char getchar() {
 		return Get().__getchar();
 	}
 	static int nextint() {
 		return Get().__nextint();
+	}
+	static unsigned int nextuint() {
+		return Get().__nextuint();
+	}
+	static long long int nextlld() {
+		return Get().__nextlld();
+	}
+	static unsigned long long int nextulld() {
+		return Get().__nextulld();
+	}
+	static double nextlf() {
+		return Get().__nextlf();
 	}
 	static int nextstring(char* x) {
 		return Get().__nextstring(x);
@@ -140,11 +256,21 @@ private:
 	}
 	void __nextint(int x, char eow = ' ') {
 		if (x < 0) {
-			putchar('-');
+			__putchar('-');
 			x = -x;
+			if(x < 0){
+				// Special case.
+				// print 2,147,483,648
+				__putchar('2');
+				__putchar('1');__putchar('4');__putchar('7');
+				__putchar('4');__putchar('8');__putchar('3');
+				__putchar('6');__putchar('4');__putchar('8');
+				__putchar(eow);
+				return;
+			}
 		}
 		if (x == 0) {
-			putchar('0');
+			__putchar('0');
 		}
 		else {
 			char xx[20];
@@ -154,20 +280,169 @@ private:
 				x /= 10;
 			}
 			while (xl--) {
-				putchar(xx[xl]);
+				__putchar(xx[xl]);
 			}
 		}
 		if (eow > 0)
-			putchar(eow);
+			__putchar(eow);
+	}
+	void __nextuint(unsigned int x, char eow = ' ') {
+		if (x == 0) {
+			__putchar('0');
+		}
+		else {
+			char xx[20];
+			int xl = 0;
+			while (x > 0) {
+				xx[xl++] = x % 10 + '0';
+				x /= 10;
+			}
+			while (xl--) {
+				__putchar(xx[xl]);
+			}
+		}
+		if (eow > 0)
+			__putchar(eow);
+	}
+	void __nextlld(long long int x, char eow = ' ') {
+		if (x < 0) {
+			__putchar('-');
+			x = -x;
+			if(x < 0){
+				// Special case.
+				// print 9,223,372,036,854,775,807
+				__putchar('9');
+				__putchar('2');__putchar('2');__putchar('3');
+				__putchar('3');__putchar('7');__putchar('2');
+				__putchar('0');__putchar('3');__putchar('6');
+				__putchar('8');__putchar('5');__putchar('4');
+				__putchar('7');__putchar('7');__putchar('5');
+				__putchar('8');__putchar('0');__putchar('8');
+				__putchar(eow);
+				return;
+			}
+		}
+		if (x == 0) {
+			__putchar('0');
+		}
+		else {
+			char xx[20];
+			int xl = 0;
+			while (x > 0) {
+				xx[xl++] = x % 10 + '0';
+				x /= 10;
+			}
+			while (xl--) {
+				__putchar(xx[xl]);
+			}
+		}
+		if (eow > 0)
+			__putchar(eow);
+	}
+	
+	void __nextulld(unsigned long long int x, char eow = ' ') {
+		if (x == 0) {
+			__putchar('0');
+		}
+		else {
+			char xx[20];
+			int xl = 0;
+			while (x > 0) {
+				xx[xl++] = x % 10 + '0';
+				x /= 10;
+			}
+			while (xl--) {
+				__putchar(xx[xl]);
+			}
+		}
+		if (eow > 0)
+			__putchar(eow);
+	}
+	void __nextlf(double x, char eow = ' ', int xl = 9)
+	{
+		// print double value until under xl.
+		// xl's point should be round up
+		// Since we cannot print directly, store it on temperal list
+		char* tmp = new char[xl + 40];
+		int i = 0;
+		int xcnt = 0;
+		int pi = -1;
+		bool minus = false;
+		bool carry_one = false;
+		if(x < 0){
+			minus = true;
+			x = -x;
+		}
+		long long int ix = static_cast<long long int>(x);
+		while(ix>0&&ix>x)ix--;
+		x-=ix;
+		if(ix == 0){
+			tmp[i++] = '0';
+			pi = i;
+		}
+		else{
+			while(ix>0){
+				tmp[i++] = (ix%10)+'0';
+				ix/=10;
+			}
+			//reverse
+			for(int j=0;j<i-j;j++){
+				char tt=tmp[j];
+				tmp[j]=tmp[i-j-1];
+				tmp[i-j-1]=tt;
+			}
+			pi = i;
+		}
+		while(xcnt < xl)
+		{
+			xcnt++;
+			x *= 10;
+			int txi = static_cast<int>(x);
+			if(txi > 0 && txi > x) txi--;
+			tmp[i++] = txi + '0';
+			tmp[i] = 0;
+			x -= txi;
+		}
+		// round up
+		{
+			x *= 10;
+			int txi = static_cast<int>(x);
+			if(txi > 0 && txi > x) txi--;
+			if((!minus && txi >= 5) || (minus && x > 5))
+			{
+				tmp[i-1]++;
+				for(int j=i-1;j>=0;j--){
+					if(tmp[j] > '9'){
+						if(j==0){carry_one = true;}
+						else{
+							tmp[j-1] += (tmp[j] - '0')/10;
+						}
+						tmp[j] -= 10;
+					}
+				}
+			}
+		}
+		if(minus) __putchar('-');
+		if(carry_one) __putchar('1');
+		if(pi == i){
+			__nextstring(tmp, eow, i);
+		}
+		else{
+			__nextstring(tmp, '.', pi);
+			__nextstring(tmp+pi, eow, i-pi);
+		}
+
+		// dealloc
+		delete[] tmp;
 	}
 	void __nextstring(char* x, char eow = ' ', int xl = -1)
 	{
-		for(int i=0; (xl>=0 && i<xl) || !isEmpty(x[i]); i++)
+		for(int i=0; (xl >= 0 && i < xl) || (xl < 0 && !isEmpty(x[i])); i++)
 		{
-			putchar(x[i]);
+			__putchar(x[i]);
 		}
 		if (eow > 0)
-			putchar(eow);
+			__putchar(eow);
 	}
 
 public:
@@ -185,6 +460,22 @@ public:
 	static void nextint(int x, char eow = ' ')
 	{
 		Get().__nextint(x, eow);
+	}
+	static void nextuint(unsigned int x, char eow = ' ')
+	{
+		Get().__nextuint(x, eow);
+	}
+	static void nextlld(long long int x, char eow = ' ')
+	{
+		Get().__nextlld(x, eow);
+	}
+	static void nextulld(unsigned long long int x, char eow = ' ')
+	{
+		Get().__nextulld(x, eow);
+	}
+	static void nextlf(double x, char eow = ' ', int xl = 9)
+	{
+		Get().__nextlf(x, eow, xl);
 	}
 	static void nextstring(char* x, char eow = ' ', int xl = -1)
 	{
